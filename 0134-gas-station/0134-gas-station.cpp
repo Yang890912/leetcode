d923943fcf9fea33f174d8dc409d4dea531daae4
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-03-18 22:35:03 +0800

    Time: 1 ms (48.25%), Space: 112.4 MB (42.44%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -4,19 +4,19 @@ public:
         int start = 0;
         int currPos = start;
         int currGas = 0;
-        int remain = 0;
+        int remainCost = 0;
 
         while(start < gas.size()) {
             currGas += gas[currPos];
             currGas -= cost[currPos];
 
             if(currPos == gas.size() - 1) {
-                if(currGas + remain < 0) return -1;
+                if(currGas - remainCost < 0) return -1;
                 else return start;
             }
 
             if(currGas < 0) {
-                remain += currGas;
+                remainCost -= currGas;
                 currGas = 0;
                 start = currPos + 1;
                 currPos = start;
