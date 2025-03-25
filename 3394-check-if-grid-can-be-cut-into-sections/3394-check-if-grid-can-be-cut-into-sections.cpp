cecb2e9cd95d1744848d8438e7316ac4e2254d28
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-03-25 21:20:20 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<vector<int>> xs;
+        vector<vector<int>> ys;
+
+        for(int i = 0 ; i < rectangles.size(); i++) {
+            vector<int> vecX, vecY;
+            vecX.push_back(rectangles[i][0]);
+            vecX.push_back(rectangles[i][2]);
+            xs.push_back(vecX);
+            vecY.push_back(rectangles[i][1]);
+            vecY.push_back(rectangles[i][3]);
+            ys.push_back(vecY);
+        }
+
+        sort(xs.begin(), xs.end(), cmp);
+        sort(ys.begin(), ys.end(), cmp);
+        int lines = 0;
+        for(int i = 1 ; i < xs.size(); i++) {
+            if(xs[i][0] < xs[i - 1][1]) continue;
+            lines++;
+            if(lines >= 2) return true;
+        }
+        lines = 0;
+        for(int i = 1 ; i < ys.size(); i++) {
+            if(ys[i][0] < ys[i - 1][1]) continue;
+            lines++;
+            if(lines >= 2) return true;
+        }
+
+        return false;
+    }
+
+    static bool cmp(const vector<int>& a, const vector<int>& b) {
+        if(a[0] == b[0]) return a[1] > b[1];
+        return a[0] < b[0];
+    }
+};
