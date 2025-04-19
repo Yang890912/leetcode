91b44a40e76d56882e32d5d7a483578b8956f9e8
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-04-19 13:01:39 +0800

    Time: 5 ms (41.52%), Space: 12.2 MB (18.32%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+        // string s = countAndSay(n - 1);
+        return vecToStr(strToVec(countAndSay(n - 1)));
+    }
+
+    vector<pair<int, int>> strToVec(string s) {
+        vector<pair<int, int>> vec;
+        int freq = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(i == 0 || s[i - 1] == s[i]) freq++;
+            else {
+                vec.push_back({freq, s[i - 1] - '0'});
+                freq = 1;
+            }
+        }
+        vec.push_back({freq, s[s.size() - 1] - '0'});
+        return vec;
+    }
+
+    string vecToStr(vector<pair<int, int>> vec) {
+        string s = "";
+        for(int i = 0; i < vec.size(); i++) {
+            s += to_string(vec[i].first);
+            s += to_string(vec[i].second);
+        }
+        return s;
+    }
+
+    
+};
